a9d9fe4dc4c821b83bf2b32377176075a03a1f2d
RemieJanssen <remiejanssen92@gmail.com>
2023-05-28 14:50:15 +0200

    restructure and add conda source file (WIP)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from tools.rooting.rooting_page import orientation_routes
-from tools.draw_graphs import drawing_routes
+from tools.drawing.draw_graphs import drawing_routes
 
 app = Flask(__name__)
 app.register_blueprint(orientation_routes)
